d114ce8df0a937c571fc8d6ba1799ef062b3321d
porebaoliwia <161926523+porebaoliwia@users.noreply.github.com>
2024-06-01 07:56:58 +0200

    add interface

---
+++ Bookstore/ClientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Konto()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Bookstore/SoldBookController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class SoldBookController : Controller
+    {
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Rejestr()
+        {
+            return View();
+        }
+
+    }
+}
